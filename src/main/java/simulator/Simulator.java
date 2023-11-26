cd25c716e07f3e081ab4de46ef198e07ff93bb6b
Andy <klee682@aucklanduni.ac.nz>
2023-11-26 22:26:43 +1300

    finished most of the cars functionality and information that will be sent to the neural network such as acceleration, velocity, the 7 sensor and point positions, need to now implement the neural network

--- src/main/java/simulator/Simulator.java
+++ src/main/java/simulator/Simulator.java
@@ -17,25 +17,27 @@ public class Simulator extends JFrame {
     private double carVelocity = 0; // Car speed
     private double carAcceleration = 0; // Car acceleration
     private double carAngle = 0; // Car angle in degrees
+    private final int baseCarTurnRate = 10; // Car turning speed
     private final double maxVelocity = 10; // Maximum velocity
-    private final double accelerationRate = 0.1; // Acceleration rate
-    private final double velocityDecayRate = 0.3; // Velocity decay rate
+    private final double accelerationRate = 0.075; // Acceleration rate
+    private final double velocityDecayRate = 0.5; // Velocity decay rate
     private final double friction = 0.02; // Friction to simulate deceleration
-    private final int carTurnRate = 5; // Car turning speed
     private final int trackWidth = 1920;
     private final int trackHeight = 1080;
     private final int carWidth = 40;
     private final int carHeight = 20;
-    private final int delay = 1; // Delay in milliseconds for the timer
+    private final int delay = 10; // Delay in milliseconds for the timer
 
     private List<Wall> raceCourse;
     private List<Point> barriers;
+    private List<Point> sensorCollisionPoints;
 
     public Simulator() {
         setTitle("Car Racing Game");
         setSize(trackWidth, trackHeight);
         setResizable(true);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        sensorCollisionPoints = new ArrayList<>();
 
         carX = trackWidth / 2.0;
         carY = trackHeight / 2.0;
@@ -54,7 +56,6 @@ public class Simulator extends JFrame {
             public void keyPressed(KeyEvent e) {
                 panel.handleKeyPress(e);
             }
-
             @Override
             public void keyReleased(KeyEvent e) {
                 panel.handleKeyRelease(e);
@@ -112,6 +113,7 @@ public class Simulator extends JFrame {
         public void handleKeyPress(KeyEvent e) {
             int key = e.getKeyCode();
 
+                    
             switch (key) {
                 case KeyEvent.VK_W:
                     carAcceleration = accelerationRate;
@@ -120,24 +122,28 @@ public class Simulator extends JFrame {
                     carAcceleration = -accelerationRate;
                     break;
                 case KeyEvent.VK_A:
-                if (carVelocity != 0){
-                    carAngle -= carTurnRate;
-                }                   
-                 break;
+                    if (carVelocity != 0) {
+                        // Adjust the turning rate based on the car's velocity
+                        double adjustedTurnRate = baseCarTurnRate * (Math.abs(carVelocity * 0.8) / maxVelocity);
+                        carAngle -= adjustedTurnRate; 
+                    }
+                    break;
                 case KeyEvent.VK_D:
-                if (carVelocity != 0){
-                    carAngle += carTurnRate;
-                }
+                    if (carVelocity != 0) {
+                        // Adjust the turning rate based on the car's velocity
+                        double adjustedTurnRate = baseCarTurnRate * (Math.abs(carVelocity * 0.8) / maxVelocity);
+                        carAngle += adjustedTurnRate;
+                    }
                     break;
                 case KeyEvent.VK_SPACE:
-                if (carVelocity > 0){
-                    carVelocity -= velocityDecayRate;
-                }else if (carVelocity < 0){
-                    carVelocity += velocityDecayRate;
-                   
-                }
-                    default:
-                break;
+                    if (carVelocity > 0) {
+                        carVelocity -= velocityDecayRate;
+                    } else if (carVelocity < 0) {
+                        carVelocity += velocityDecayRate;
+                    }
+                    break;
+                default:
+                    break;
             }
         }
 
@@ -147,11 +153,12 @@ public class Simulator extends JFrame {
             switch (key) {
                 case KeyEvent.VK_W:
                 case KeyEvent.VK_S:
-                case KeyEvent.VK_SPACE:
                     carAcceleration = 0;
                     break;
+
             }
         }
+        
 
         public void moveCar() {
             // Update velocity based on acceleration
@@ -169,6 +176,9 @@ public class Simulator extends JFrame {
             if (carVelocity < 0.05 && carVelocity > -0.05){
                 carVelocity = 0;
             }
+
+            calculateSensorCollisionPoints();
+
         
             // Limit velocity to maxVelocity
             carVelocity = Math.min(maxVelocity, Math.max(-maxVelocity, carVelocity));
@@ -183,31 +193,126 @@ public class Simulator extends JFrame {
                 int barrierRadius = 5; // You can adjust this value based on your needs
             
                 if (Math.abs(carX - barrier.x) <= barrierRadius && Math.abs(carY - barrier.y) <= barrierRadius) {
+
+                    if (carVelocity > 0){
+                        carX += Math.cos(angleInRadians);
+                        carY += Math.sin(angleInRadians);
+                    }
+                    else if (carVelocity < 0){
+                        carX -= Math.cos(angleInRadians);
+                        carY -= Math.sin(angleInRadians);
+                    }
                     carVelocity = 0;
+                    carAcceleration = 0;
                     System.out.println("Collision detected!");
 
-                    // move the car back a bit to avoid repeated collision detection
-                    carX -= Math.cos(angleInRadians);
-                    carY -= Math.sin(angleInRadians);
                 }
             }
 
             
 
-            // If the car goes off the screen, move it to the other side
-            if (carX < 0 || carX > trackWidth || carY < 0 || carY > trackHeight) {
-                carX = (carX + trackWidth) % trackWidth;
-                carY = (carY + trackHeight) % trackHeight;
+            // If the car goes off the screen
+            if (carX < 0) {
+                carVelocity = 0;
+                carX = 0;
+            } else if (carX > trackWidth - carWidth) {
+                carVelocity = 0;
+                carX = trackWidth - carWidth;
+            }
+            
+            if (carY < 0) {
+                carVelocity = 0;
+                carY = 0;
+            } else if (carY > trackHeight - carHeight) {
+                carVelocity = 0;
+                carY = trackHeight - carHeight;
             }
     
             repaint(); 
         }
 
+        private void calculateSensorCollisionPoints() {
+            sensorCollisionPoints.clear();
+        
+            int numSensors = 7; // Adjust the number of sensors as needed
+            double startSensorAngle = Math.toRadians(-45); // Start angle for the first sensor
+            double endSensorAngle = Math.toRadians(45); // End angle for the last sensor
+            double angleIncrement = (endSensorAngle - startSensorAngle) / (numSensors - 1); // Angle increment between sensors
+            double angleInRadians = Math.toRadians(carAngle);
+        
+            for (int i = 0; i < numSensors; i++) {
+                double sensorX = carX + carWidth / 2.0;
+                double sensorY = carY + carHeight / 2.0;
+        
+                double sensorDistance = 200; // Adjust the sensor distance as needed
+        
+                double sensorEndX = sensorX + sensorDistance * Math.cos(angleInRadians + startSensorAngle);
+                double sensorEndY = sensorY + sensorDistance * Math.sin(angleInRadians + startSensorAngle);
+        
+                Point collisionPoint = calculateCollisionPoint(sensorX, sensorY, sensorEndX, sensorEndY);
+        
+
+                if (collisionPoint != null){
+                     // Check if the collision point is in front of the car
+                    double angleToCollisionPoint = Math.atan2(collisionPoint.y - sensorY, collisionPoint.x - sensorX);
+                    double angleDifference = Math.abs(Math.atan2(Math.sin(angleToCollisionPoint - angleInRadians),
+                                                                Math.cos(angleToCollisionPoint - angleInRadians)));
+            
+                    // Adjust this threshold to control the sensitivity of the sensors
+                    double angleThreshold = Math.toRadians(90);
+            
+                    if (angleDifference <= angleThreshold) {
+                        sensorCollisionPoints.add(collisionPoint);
+                    }
+
+                }
+               
+        
+                startSensorAngle += angleIncrement;
+            }
+        }
+        
+        
+        private Point calculateCollisionPoint(double startX, double startY, double endX, double endY) {
+            Point closestCollisionPoint = null;
+            double closestDistance = Double.MAX_VALUE;
+
+            for (Point barrier : barriers) {
+                if (lineIntersectsCircle(startX, startY, endX, endY, barrier.x, barrier.y, 5)) {
+                    double distance = Math.sqrt(Math.pow(barrier.x - startX, 2) + Math.pow(barrier.y - startY, 2));
+                    if (distance < closestDistance) {
+                        closestDistance = distance;
+                        closestCollisionPoint = new Point((int) barrier.x, (int) barrier.y);
+                    }
+                }
+
+            }
+
+            return closestCollisionPoint != null ? closestCollisionPoint : closestCollisionPoint;
+        }
+
+        
+        private boolean lineIntersectsCircle(double startX, double startY, double endX, double endY, double circleX, double circleY, double radius) {
+            double dx = endX - startX;
+            double dy = endY - startY;
+            double a = dx * dx + dy * dy;
+            double b = 2 * (dx * (startX - circleX) + dy * (startY - circleY));
+            double c = circleX * circleX + circleY * circleY + startX * startX + startY * startY
+                    - 2 * (circleX * startX + circleY * startY) - radius * radius;
+        
+            return b * b - 4 * a * c >= 0;
+        }
+
+
+
         @Override
         protected void paintComponent(Graphics g) {
             super.paintComponent(g);
             drawTrack(g);
+            drawSensorLines(g);
             drawCar(g);
+            drawSpedometer(g);
+            drawSensorPos(g);
         }
 
         private void drawTrack(Graphics g) {
@@ -219,6 +324,36 @@ public class Simulator extends JFrame {
             g.setColor(Color.WHITE);
             for (Wall wall : raceCourse) {
                 g.drawLine(wall.start.x, wall.start.y, wall.end.x, wall.end.y);
+            } 
+        }
+
+        private void drawSensorLines(Graphics g) {
+            g.setColor(Color.YELLOW);
+            for (Point sensorCollisionPoint : sensorCollisionPoints) {
+                if (sensorCollisionPoint != null){
+                    g.drawLine((int) carX + carWidth / 2, (int) carY + carHeight / 2,
+                    sensorCollisionPoint.x, sensorCollisionPoint.y);
+                }
+            }
+            
+        }
+
+        private void drawSpedometer(Graphics g) {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+            String formattedVelocity = String.format("%.1f", carVelocity * 10);
+            g.drawString("Speed: " +  formattedVelocity + " KM/H", 10, 20);
+        }
+
+
+        private void drawSensorPos(Graphics g) {
+            g.setColor(Color.WHITE);
+            g.setFont(new Font("TimesRoman", Font.PLAIN, 20));
+            int y = 40;
+            for (Point sensor: sensorCollisionPoints) {
+                g.drawString("X: " + sensor.x + " Y: " + sensor.y, 1700, y);
+
+                y += 25;
             }
         }
 
